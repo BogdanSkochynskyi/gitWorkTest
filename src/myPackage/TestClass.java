7346b27e2d726ce444af1618f8738ba053108dd2
bogdanskochynskyi@gmail.com <bogdanskochynskyi@gmail.com>
2016-09-11 15:49:00 +0300

    update text

--- src/myPackage/TestClass.java
+++ src/myPackage/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("First commit on master");
 
-        System.out.println("Bla bla bla");
+        System.out.println("Bla bladvdd bla");
 
     }
 }
